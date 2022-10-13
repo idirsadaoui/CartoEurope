537e110a0d3b0df84842c1ba5c581a3e1d5ead68
Idir Sadaoui <102378096+idirsadaoui@users.noreply.github.com>
2022-10-13 15:37:04 +0200

    ttest
    test

---
+++ DATASET.R
@@ -0,0 +1,16 @@
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data(DATASET, overwrite = TRUE)
+
+usethis::use_data(cartogram_EU,compress = "xz")
+
+usethis::use_data(THOR,compress = "xz")
+
+usethis::use_data(wrld_simpl,compress = "xz")
+
+setwd("~/Desktop/CartoEurope")
+
+library(CartoEurope)
+
+data(CartoEurope::wrld_simpl)
+library(CartoEurope)
